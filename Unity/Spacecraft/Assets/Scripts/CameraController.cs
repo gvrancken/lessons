e3ee02917ff641b8ccd80a9c5d5d10f61fc26b76
Merijn Vogelsang <merijn@pillowswillow.com>
2018-01-11 23:02:24 +0100

    Add Unity 2017 Spacecraft project

---
+++ Unity/Spacecraft/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    private float m_RotationSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float AxisHor = Input.GetAxis("Cam_Horizontal");
+        float axisVer = Input.GetAxis("Cam_Vertical");
+
+        Debug.Log(transform.localEulerAngles.x + " " + axisVer);
+        if (transform.localEulerAngles.x > 300f && transform.localEulerAngles.x < 325f && axisVer < 0f)
+        {
+            axisVer = 0;
+        }
+        else if (transform.localEulerAngles.x > 45f && transform.localEulerAngles.x < 60f && axisVer > 0)
+        {
+            axisVer = 0;
+        }
+
+
+        transform.Rotate(new Vector3(0f , AxisHor * Time.deltaTime * m_RotationSpeed, 0f), Space.World);
+        transform.Rotate(new Vector3(axisVer * Time.deltaTime * m_RotationSpeed, 0f, 0f), Space.Self);
+
+	}
+}
